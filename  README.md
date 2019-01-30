abca44427a12176d447e54e210ce8e7b72012eae
markomihajlovic <marko4400@gmail.com>
2019-01-30 10:18:42 +0100

    Add files via upload

---
+++  README.md
@@ -0,0 +1,13 @@
+09:27
+they've never seen me before so I've made this mistake before and you guys
+09:31
+know a resume correlates loosely with whether or not you know how to do a job
+09:37
+if your resume shows for sure that you could do a job you'd never lose one
+09:44
+they'd never be a bad hire they'd never be an employee that didn't work out
+09:48
+correlates loosely with whether or not you know how to do the job so do I say
+
+source:
+https://www.youtube.com/embed/llctqNJr2IU?start=564&end=594&version=3
---
+++ SolvesTheUniverse.java
@@ -0,0 +1,89 @@
+public class SolvesTheUniverse {
+   private static void print(String str) {
+try{Thread.sleep(2000);}catch(InterruptedException ex) {print("baaad");}finally{
+      System.out.println(str);}
+   } 
+
+   public static void main(String []args) {
+      print("<TITLE>CV</TITLE>");
+      print("<DATE>24.04.2018</DATE>");
+      print("<FIRSTNAME>MARKO</FIRSTNAME>");
+      print("<LASTNAME>MIHAJLOVIC</LASTNAME>");
+      print("<MAILTO>marko4400@gmail.com</MAILTO>");
+      print("<PHONE>+36305544028</PHONE>");
+	  
+      print("<JOBS>");
+      print("<JOB>T-SYSTEM <START>2016 SEPTEMBER</START><END>2017 JUNE</END><LANGUAGE>JAVA</LANGUAGE><FIELD>MEDICAL</FIELD></JOB>");
+      print("<JOB>NETVERTISE <START>2016 FEB</START><END>APR</END> <FIELD>VIDEO STREAMING, TESTER</FIELD></JOB>");
+      print("<JOB>iAkos <START>2015 NOVEMBER</START><END>2016 JAN</END><LANGUAGE>JAVA</LANGUAGE><FIELD>CASINO ADMINISTRATION SOFTWARE</FIELD></JOB>");
+      print("<JOB>LILIGO <START>2014 JULY</START><END>2014 AUG</END><LANGUAGE>JAVA</LANGUAGE><LANGUAGE>JAVASCRIPT</LANGUAGE><LANGUAGE>PYTHON</LANGUAGE><FIELD>AIRPLANE TICKET AUTOMATION TESTING</FIELD></JOB>");
+
+      print("<BR>");
+      print("<JOB>ANTIQUE<FIELD>DEALING WITH PEOPLE</FIELD></JOB>");
+      print("</JOBS>");
+
+      print("<BR><BR>");
+
+      print("<LANGUAGES>");
+	      print("<LANGUAGE>OBJECTIVE-C");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>SWIFT");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>JAVA");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>C");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>C++");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>C#");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>SQL");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>PYTHON");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>PHP");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>JAVASCRIPT");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>ACTIONSCRIPT");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>ASSEMBLY");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>BRAINF*");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>SQL");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>HTML");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>HUNGARIAN");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>SERBIAN");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>ENGLISH");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>ITALIAN");
+	      print("</LANGUAGE>");
+	      print("<LANGUAGE>VIETNAMESE");
+	      print("</LANGUAGE>");
+      print("</LANGUAGES>");
+
+      print("<HOBBIES>");
+	      print("<ITEM>MEMORY GAMES");
+	      print("</ITEM>");
+	      print("<ITEM>BLENDER");
+	      print("</ITEM>");
+	      print("<ITEM>UNITY");
+	      print("</ITEM>");
+	      print("<ITEM>STUDYING");
+	      print("</ITEM>");
+	      print("<ITEM>ARDUINO");
+	      print("</ITEM>");
+	      print("<ITEM>STACKOVERFLOW");
+	      print("</ITEM>");
+	      print("<ITEM>ROSETTACODE");
+	      print("</ITEM>");
+	      print("<ITEM>PHYSICS");
+	      print("</ITEM>");
+      print("</HOBBIES>");
+   }
+}
